cd8ed6eadf15031cbe0517db080e52bf7cbe72e4
Destiny <dshillis1@gmail.com>
2019-05-10 17:22:02 -0600

    Front-end Make Search connecting

--- front-end/src/BoardContainer/BoardContainer.jsx
+++ front-end/src/BoardContainer/BoardContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MakeBoard from './MakeBoard/MakeBoard';
+import Search from './MakeBoard/Search';
 
 const BoardContainer = () => {
     return (
         <div>
         <h1>BoardContainer</h1>
         <MakeBoard />
+        <Search />
         </div>
     )
 }
---
+++ front-end/src/BoardContainer/MakeBoard/Search.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    constructor(){
+        super();
+        this.state = {
+            
+        }
+    }
+    render(){
+        return (
+            <h1>Search</h1>
+        )
+    }
+
+}
+
+export default Search;
