fd192846496463ab5a15f2581bc39ff5acf17da4
Destiny <dshillis1@gmail.com>
2019-05-10 17:21:07 -0600

    Front-end Make Board connecting

--- front-end/src/BoardContainer/BoardContainer.jsx
+++ front-end/src/BoardContainer/BoardContainer.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import MakeBoard from './MakeBoard/MakeBoard';
 
 const BoardContainer = () => {
     return (
+        <div>
         <h1>BoardContainer</h1>
+        <MakeBoard />
+        </div>
     )
 }
 
---
+++ front-end/src/BoardContainer/MakeBoard/MakeBoard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MakeBoard extends Component {
+    constructor(){
+        super();
+        this.state = {
+            
+        }
+    }
+    render(){
+        return (
+            <h1>MakeBoard</h1>
+        )
+    }
+
+}
+
+export default MakeBoard;
