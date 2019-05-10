e5208aac750943b847080b640df586c84738b5ac
Destiny <dshillis1@gmail.com>
2019-05-10 17:18:34 -0600

    Front-end Board Container working

--- back-end/controllers/BoardController.js
+++ back-end/controllers/BoardController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 // index
 router.get('/', (req, res) => {
-    res.send('hit')
+    
 })
 
 module.exports = router;
---
+++ front-end/src/BoardContainer/BoardContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BoardContainer = () => {
+    return (
+        <h1>BoardContainer</h1>
+    )
+}
+
+export default BoardContainer;
