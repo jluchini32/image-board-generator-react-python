5d88941cfc2282f3d47aa94f8b249a95807fdc3c
Destiny <dshillis1@gmail.com>
2019-05-10 17:23:11 -0600

    Front-end Make Profile connecting

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import BoardContainer from './BoardContainer/BoardContainer';
+import Profile from './Profile/Profile';
 
 function App() {
   return (
     <div className="App">
       <BoardContainer />
+      <Profile />
     </div>
   );
 }
